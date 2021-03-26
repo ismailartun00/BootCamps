c84f7f458f347ae72da14d69ba0ccf6a3546eaf0
ismail <iartun25@gmail.com>
2021-03-26 20:17:51 +0300

    26.03.2021

--- KampIntro/9-DegerVeReferansTipler/Program.cs
+++ KampIntro/9-DegerVeReferansTipler/Program.cs
@@ -13,7 +13,7 @@ namespace _9_DegerVeReferansTipler {
             int[] sayilar2 = new int[] { 100, 200, 300 };
             sayilar1 = sayilar2;
             sayilar2[0] = 999;
-            //Console.WriteLine(sayilar1[0]);     --> 999
+            //Console.WriteLine(sayilar1[0]);     --> 999     
 
 
         }
