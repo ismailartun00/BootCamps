e077262f0587fda445bcff284e90dd1372d69e6f
ismail <ismailartun00@gmail.com>
2021-05-23 06:42:25 +0300

    java - Ract kamp 7.gün

--- Java - React/northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
+++ Java - React/northwind/src/main/java/kodlamaio/northwind/api/controllers/ProductsController.java
@@ -4,30 +4,36 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.Result;
 import kodlamaio.northwind.entities.concretes.Product;
 
-@RestController		// controllersın dedik.
-@RequestMapping("/api/products")		// eğer domaimize ek olarak bu adres girilirse burası karşılayacak diyoruz.
+@RestController // controllersın dedik.
+@RequestMapping("/api/products") // eğer domaimize ek olarak bu adres girilirse burası karşılayacak diyoruz.
 public class ProductsController {
-	
-	
+
 	private ProductService productService;
-	
-	
-	@Autowired		// parametre tipine bakarak projeyi tarıyor ve bu parametreyi implemente eden class'ı bulup onu new'liyor.
+
+	@Autowired // parametre tipine bakarak projeyi tarıyor ve bu parametreyi implemente eden class'ı bulup onu new'liyor.
 	public ProductsController(ProductService productService) {
 		super();
 		this.productService = productService;
 	}
 
-
-
-	@GetMapping("/getall")		// GetMapping veri istiyoruz demek ve api/products/'dan sonra /getall dersek bu metot çalışacak.
-	public List<Product> getAll(){
+	@GetMapping("/getall") // GetMapping veri istiyoruz demek ve api/products/'dan sonra /getall dersek bu metot çalışacak.
+	public DataResult<List<Product>> getAll() {
 		return this.productService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Product product) {
+		
+		return this.productService.add(product);
 	}
 }
--- Java - React/northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ Java - React/northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,18 +3,23 @@ package kodlamaio.northwind.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data		// getter setter fonksiyonları otomatik oluşturur.
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity		// veri tabanı nesnesisin demektir.
 @Table(name="products")		// veri tabanında hangi tabloya karşılık geldiğini söyleriz.
 public class Product {
 	
 	    @Id		// tablodaki primary key alanımızı belirtmek için kullanıyoruz.
-	    @GeneratedValue		// id'miz bir bir arttırılıyor.
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)		// id'miz bir bir arttırılıyor.
 	    @Column(name="product_id")		// veri tabanındaki hangi kolona karşılık geldiğini yazıyoruz.
 		private int id;
 	    
@@ -32,16 +37,5 @@ public class Product {
 	    
 	    @Column(name="quantity_per_unit")
 		private String quantityPerUnit;
-	    
-	    public Product() {}
-		
-		public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock, String quantityPerUnit) {
-			this.id = id;
-			this.categoryId = categoryId;
-			this.productName = productName;
-			this.unitPrice = unitPrice;
-			this.unitsInStock = unitsInStock;
-			this.quantityPerUnit = quantityPerUnit;
-		}
 				
 }
